0c0ee114f6e27b77e8f628175fde315bea387038
leo9827 <leo.lihong@foxmail.com>
2025-12-10 20:40:25 +0800

    feat: update dashboard

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -21,8 +21,70 @@ return {
   },
   config = function()
     require('alpha').setup(require('alpha.themes.theta').config)
-    -- require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    -- require'alpha'.setup(require'alpha.themes.startify'.config)
-    vim.api.nvim_create_user_command('Dashboard', 'Alpha', { desc = '打开 Alpha 启动页面(Dashborad).' })
+    vim.api.nvim_create_user_command('Dashboard', 'Alpha', { desc = 'Open Dashborad.' })
+
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    local function command_exists(cmd)
+      local handle = io.popen('which ' .. cmd .. ' 2>/dev/null')
+      if handle then
+        local result = handle:read '*a'
+        handle:close()
+        return result ~= ''
+      end
+      return false
+    end
+
+    local function get_fortune()
+      if command_exists 'fortune' and command_exists 'cowsay' then
+        local handle = io.popen "fortune -s | cowsay -f $(cowsay -l | tail -n +2 | tr ' ' '\\n' | gshuf -n1)"
+        if handle then
+          local result = handle:read '*a'
+          handle:close()
+          return result
+        end
+      end
+
+      -- if command not found, return default
+      return [[
+   ╭─────────────────────────────╮
+   │         Welcome back!       │
+   ╰─────────────────────────────╯
+                        ]]
+    end
+
+    dashboard.section.header.val = vim.split(get_fortune(), '\n')
+    dashboard.section.header.opts.hl = 'AlphaHeader'
+
+    dashboard.section.buttons.val = {
+      dashboard.button('n', '  New File', ':ene <BAR> startinsert<CR>'),
+      dashboard.button('SPC f r', '  Recent Files', ':Telescope oldfiles<CR>'),
+      dashboard.button('SPC s f', '  Find Files', '<leader>sf'),
+      dashboard.button('SPC f g', '  Fuzzy Grep', '<leader>sg'),
+      dashboard.button('SPC f g', '  Update Plugins', ':Lazy update<CR>'),
+      dashboard.button('q', '  Quit', '<cmd>q!<cr>'),
+    }
+
+    local function get_day_of_week()
+      return os.date '%A'
+    end
+
+    dashboard.section.footer.val = {
+      get_day_of_week(),
+    }
+    dashboard.section.footer.opts.hl = 'AlphaFooter'
+
+    dashboard.config.layout = {
+      { type = 'padding', val = 1 },
+      dashboard.section.header,
+      { type = 'padding', val = 2 },
+      dashboard.section.buttons,
+      { type = 'padding', val = 1 },
+      dashboard.section.footer,
+    }
+
+    alpha.setup(dashboard.config)
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
   end,
 }
