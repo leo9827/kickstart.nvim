fef35f394a19427ff479d1e2e772da89e3dc40b7
leo9827 <leo.lihong@foxmail.com>
2026-01-04 18:02:30 +0800

    feat: change oldfiles search current dir

--- init.lua
+++ init.lua
@@ -489,7 +489,9 @@ require('lazy').setup({ -- NOTE: Plugins can be added with a link (or for a gith
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s.', function()
+        builtin.oldfiles { cwd_only = true }
+      end, { desc = '[S]earch Recent Files in Current Dir ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
