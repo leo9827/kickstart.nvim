fb5de7bb20b0b7c3b56a0102fb0b72792d152c73
leo9827 <leo.lihong@foxmail.com>
2026-05-13 22:25:05 +0800

    feat: adjust dashboard shortcut position

--- lua/custom/plugins/ui-enhancement/alpha-nvim.lua
+++ lua/custom/plugins/ui-enhancement/alpha-nvim.lua
@@ -64,8 +64,8 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button('r', '  Recent Files', ':Telescope oldfiles only_cwd=true<CR>'),
       dashboard.button('f', '  Find Files', ':Telescope find_files<CR>'),
-      dashboard.button('d', '  Git Changes', ':Telescope git_status<CR>'),
       dashboard.button('g', '  Fuzzy Grep', ':Telescope live_grep<CR>'),
+      dashboard.button('d', '  Git Changes', ':Telescope git_status<CR>'),
       dashboard.button('s', '  Restore Session', ':SessionRestore<CR>'),
       dashboard.button('n', '  New File', ':ene <BAR> startinsert<CR>'),
       dashboard.button('c', '  Configuration', ':e $MYVIMRC<CR>'),
